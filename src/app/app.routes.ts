e5b2c78a1170f5b26d5cf9a4c9b8991a1e50795d
chk363 <gitchkip@aol.com>
2025-05-01 22:35:38 +0200

    add landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {BookListComponent} from './book-list/book-list.component';
+import {LandingPageComponent} from './landing-page/landing-page.component';
 
 export const routes: Routes = [
   { path: 'books', component: BookListComponent },
+  { path: '', component: LandingPageComponent}
 ];
