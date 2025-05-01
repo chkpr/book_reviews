5ff4c2fb43644b6b02507b402fea5791cf11592d
chk363 <gitchkip@aol.com>
2025-05-01 12:47:08 +0200

    add book info layout

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-book',
@@ -6,6 +6,24 @@ import { Component } from '@angular/core';
   templateUrl: './book.component.html',
   styleUrl: './book.component.scss'
 })
-export class BookComponent {
+export class BookComponent implements OnInit {
+  title!: string;
+  author!: string;
+  publisher!: string;
+  publishing!: string;
+  image!: string;
+  description!: string;
+  createdAt!: Date;
+  likes!: number;
 
+  ngOnInit() {
+    this.title = 'Silo';
+    this.author = 'Hugh Howey';
+    this.publisher = 'Actes Sud';
+    this.publishing = '2013'
+    this.image = 'silo.jpg'
+    this.description = '"Dans un futur indéterminé, des survivants vivent depuis plusieurs générations dans un immense silo creusé dans la terre, à l\'abri d\'une atmosphère devenue toxique."';
+    this.createdAt = new Date();
+    this.likes = 5;
+  }
 }
