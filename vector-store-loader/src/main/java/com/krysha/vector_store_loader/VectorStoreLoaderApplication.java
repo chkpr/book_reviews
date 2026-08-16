6d7c98977b296f8b4b9da93abf1c1d4671631ee9
chkpr <156218704+chkpr@users.noreply.github.com>
2026-08-16 23:02:41 +0200

    feat: add document loader

---
+++ vector-store-loader/src/main/java/com/krysha/vector_store_loader/VectorStoreLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.krysha.vector_store_loader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VectorStoreLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VectorStoreLoaderApplication.class, args);
+	}
+
+}
