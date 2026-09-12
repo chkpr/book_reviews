df4a00e3dbfbe503cc1b94cfc62c7f5f033b6945
chkpr <156218704+chkpr@users.noreply.github.com>
2026-09-12 23:02:18 +0200

    feat: configure chat memory

--- backend/src/main/java/com/krysha/bookreview/config/AiConfig.java
+++ backend/src/main/java/com/krysha/bookreview/config/AiConfig.java
@@ -5,6 +5,8 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.ChatMemoryRepository;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
 import org.springframework.ai.rag.preretrieval.query.transformation.RewriteQueryTransformer;
 import org.springframework.ai.rag.preretrieval.query.transformation.TranslationQueryTransformer;
@@ -16,6 +18,13 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AiConfig {
+	
+	  @Bean
+	  ChatMemory chatMemory(ChatMemoryRepository chatMemoryRepository) {
+	    return MessageWindowChatMemory.builder()
+	        .chatMemoryRepository(chatMemoryRepository)
+	        .build();
+	  }
 	
 	@Bean
 	ChatClient chatClient(
